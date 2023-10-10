0b5a380c10e3c393db1177de5d7fb58f52c851fe
David Prothero <david@prothero.com>
2023-10-10 12:24:16 -0700

    Fix HTML/CSS bug in monster builder

--- src/utils/fetch.ts
+++ src/utils/fetch.ts
@@ -4,7 +4,21 @@ import { DOC_URLS } from "./constants.js";
 export async function fetchDocument(docType: DocType): Promise<string> {
   process.stdout.write(`Downloading ${docType}...`);
   const response = await fetch(DOC_URLS[docType]);
-  const html = await response.text();
+  let html = await response.text();
+
+  // fix bug in first release of monster builder HTML
+  if (docType === "5e_Monster_Builder") {
+    html = html.replace(
+      `  th.nowrap, td.nowrap {
+    white-space: nowrap;
+  table {max-width: 800px}`,
+      `  th.nowrap, td.nowrap {
+    white-space: nowrap;
+  }
+  table {max-width: 800px}`
+    );
+  }
+
   process.stdout.write("Done\n");
 
   return html;
