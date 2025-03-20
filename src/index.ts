5a827e85452e0ebf3ebe921db7a38038ba247527
David Prothero <david@prothero.com>
2025-03-19 19:03:54 -0700

    Re-enable npm publish

--- src/index.ts
+++ src/index.ts
@@ -62,7 +62,7 @@ async function createOutput(docType: DocType) {
     logUpdate(docType, "json");
   }
 
-  // await publishJsonPackage(docType);
+  await publishJsonPackage(docType);
 }
 
 async function run() {
